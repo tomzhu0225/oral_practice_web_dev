e431030a842cbf4432dd79952ac15025550d1ffa
tomzhu0225 <75157161+tomzhu0225@users.noreply.github.com>
2023-02-09 11:06:18 +0100

    Update core.py

--- core.py
+++ core.py
@@ -13,28 +13,7 @@ import requests
 import json
 #lang="zh-CN"
 lang="fr-FR"
-def recognize_from_mic(lang):
-	#Find your key and resource region under the 'Keys and Endpoint' tab in your Speech resource in Azure Portal
-	#Remember to delete the brackets <> when pasting your key and region!
-    speech_config = speechsdk.SpeechConfig(subscription="5c05507933314a0caa980687fad5e2de", region="francecentral")
-    speech_config.speech_recognition_language = lang
-    audio_config = speechsdk.AudioConfig(filename='example.wav')
-    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config,audio_config=audio_config)    
-    #Asks user for mic input and prints transcription result on screen
-    
-    result = speech_recognizer.recognize_once()
-    
-    return result.text
-def synthesize_to_speaker(text,lang):
-	#Find your key and resource region under the 'Keys and Endpoint' tab in your Speech resource in Azure Portal
-	#Remember to delete the brackets <> when pasting your key and region!
-    speech_config = speechsdk.SpeechConfig(subscription="5c05507933314a0caa980687fad5e2de", region="francecentral")
-    speech_config.speech_synthesis_language = lang
-    #In this sample we are using the default speaker 
-    #Learn how to customize your speaker using SSML in Azure Cognitive Services Speech documentation
-    audio_config = AudioOutputConfig(use_default_speaker=True)
-    synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
-    synthesizer.speak_text_async(text)
+
 
 def respond(conversation,mod,key):
     openai.api_key = key
