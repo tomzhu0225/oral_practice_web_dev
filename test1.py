90de7cceeb5ddd5831997299027f9bb3dc0b95e8
tomzhu0225 <2062141317@qq.com>
2023-02-10 21:02:39 +0100

    assemb

--- test1.py
+++ test1.py
@@ -296,10 +296,10 @@ def app_sst():
                 sound_chunk = sound_chunk.set_channels(1).set_frame_rate(16000)
                 sound1=sound1+sound_chunk
                 sound_eval=sound_eval+sound_chunk
-            if i % 20 ==0 and i>150:
+            if i % 50 ==0 and i>150:
                 deci_stop =np.array(sound_eval.get_array_of_samples()) # auto stop
                 max_v=np.amax(deci_stop)
-                if max_v<700:
+                if max_v<500:
                     break
                 else:
                     sound_eval = pydub.AudioSegment.empty()
